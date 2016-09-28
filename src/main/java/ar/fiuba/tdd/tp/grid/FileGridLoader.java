1a5763c23fb6fe75aa827a938934c286e5bc0a92
LucianoRM <luciano.rmna@gmail.com>
2016-09-28 13:50:01 -0300

    Added method isComment on fileGridLoader to check for comments in files

--- src/main/java/ar/fiuba/tdd/tp/grid/FileGridLoader.java
+++ src/main/java/ar/fiuba/tdd/tp/grid/FileGridLoader.java
@@ -6,19 +6,31 @@ import ar.fiuba.tdd.tp.cell.OneDigitCell;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by luciano on 26/09/16.
  * This class lods a grid from a file
  */
-public abstract class FileGridLoader extends GridLoader {
+public class FileGridLoader extends GridLoader {
 
     protected Scanner fileScanner;
+
+    @Override
+    protected void fillGrid(Grid grid) {
+    }
 
     public FileGridLoader(String newFilename) throws FileNotFoundException {
 
         this.fileScanner = new Scanner(new File(newFilename),"UTF-8");
 
+    }
+
+    public Boolean isComment(String line){
+        Pattern regex = Pattern.compile("^#.*$");
+        Matcher match = regex.matcher(line);
+        return match.matches();
     }
 
 
