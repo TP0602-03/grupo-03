5d17d935b85f866536e3561492211473b10acde8
LucianoRM <luciano.rmna@gmail.com>
2016-09-29 00:16:32 -0300

    Adding mock cell clases

---
+++ src/main/java/ar/fiuba/tdd/tp/cell/KakuroCell.java
@@ -0,0 +1,16 @@
+package ar.fiuba.tdd.tp.cell;
+
+/**
+ * Created by luciano on 28/09/16.
+ */
+public class KakuroCell extends Cell {
+
+    public Boolean isWrittable(){ return true;}
+    public void setCenter(int value) {}
+    public int getCenter() {return 1;}
+    public int getLowerLeft(){return 1;}
+    public int getUpperRight() {return 1;}
+    public void setLowerLeft(int value){}
+    public void setUpperRight(int value){}
+
+}
