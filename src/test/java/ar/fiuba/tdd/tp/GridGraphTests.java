2cdf36cffc56be699c826b97b0086997a16eec04
chrishaig14 <chrishaig14@gmail.com>
2016-10-15 16:58:29 -0300

    arreglo warnings

--- src/test/java/ar/fiuba/tdd/tp/GridGraphTests.java
+++ src/test/java/ar/fiuba/tdd/tp/GridGraphTests.java
@@ -10,26 +10,26 @@ import static org.junit.Assert.assertEquals;
 public class GridGraphTests {
 
     @Test
-    public void gridGraphGetVertex(){
-        GridGraph grid = new GridGraph(9,9);
-        GraphVertex vertex = grid.getVertex(1,1);
-        assertEquals(grid.getVertex(1,1),vertex);
+    public void gridGraphGetVertex() {
+        GridGraph grid = new GridGraph(9, 9);
+        GraphVertex vertex = grid.getVertex(1, 1);
+        assertEquals(grid.getVertex(1, 1), vertex);
     }
 
     @Test(expected = java.lang.IndexOutOfBoundsException.class)
-    public void gridGraphGetMissingVertexReturnsIndexOutOfBoundsException(){
-        GridGraph grid = new GridGraph(9,9);
-        assertEquals(grid.getVertex(10,10),null);
+    public void gridGraphGetMissingVertexReturnsIndexOutOfBoundsException() {
+        GridGraph grid = new GridGraph(9, 9);
+        assertEquals(grid.getVertex(10, 10), null);
     }
 
     @Test
-    public void gridGraphAddEdge(){
-        GridGraph grid = new GridGraph(3,3);
-        Coord c1 = new Coord(1,1);
-        Coord c2 = new Coord(2,2);
-        grid.addEdge(c1,c2);
-        GraphVertex vertex = grid.getVertex(1,1);
-        GraphVertex vertex2 = grid.getVertex(2,2);
-        assert(vertex.getAdjacencyList().contains(vertex2));
+    public void gridGraphAddEdge() {
+        GridGraph grid = new GridGraph(3, 3);
+        Coord c1 = new Coord(1, 1);
+        Coord c2 = new Coord(2, 2);
+        grid.addEdge(c1, c2);
+        GraphVertex vertex = grid.getVertex(1, 1);
+        GraphVertex vertex2 = grid.getVertex(2, 2);
+        assert (vertex.getAdjacencyList().contains(vertex2));
     }
 }
