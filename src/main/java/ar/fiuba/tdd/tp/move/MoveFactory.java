3a76985d7d27322570ee89d443904f9e91c70422
LucianoRM <luciano.rmna@gmail.com>
2016-10-15 16:00:20 -0300

    Now MoveFactory also checks valid positions

--- src/main/java/ar/fiuba/tdd/tp/move/MoveFactory.java
+++ src/main/java/ar/fiuba/tdd/tp/move/MoveFactory.java
@@ -1,5 +1,9 @@
 package ar.fiuba.tdd.tp.move;
 
+import javafx.util.Pair;
+
+import java.awt.*;
+import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -8,24 +12,34 @@ import java.util.HashMap;
  */
 public class MoveFactory {
 
+    //Saves the possible values to be used in the move
     HashMap<String,Boolean> possibleValues;
 
-    public MoveFactory(HashMap<String,Boolean> possibleValuesMap) {
+    //Saves the possible positions for the move to take place in
+    HashMap<Pair<Integer,Integer>,Boolean> possiblePositions;
+
+    public MoveFactory(HashMap<String,Boolean> possibleValuesMap, HashMap<Pair<Integer,Integer>,Boolean> possiblePositionsMap) {
         this.possibleValues = possibleValuesMap;
+        this.possiblePositions = possiblePositionsMap;
     }
 
-    public MoveFactory(ArrayList<String> possibleValuesList) {
+    public MoveFactory(ArrayList<String> possibleValuesList,ArrayList<Pair<Integer,Integer>> possiblePositionsList) {
 
         this.possibleValues = new HashMap<>();
         for( String value : possibleValuesList) {
             this.possibleValues.put(value,Boolean.TRUE);
         }
 
+        this.possiblePositions = new HashMap<>();
+        for (Pair<Integer,Integer> position : possiblePositionsList) {
+            this.possiblePositions.put(position,Boolean.TRUE);
+        }
     }
 
     public Move createMove(int id,int x,int y,String value) {
 
-        if (this.possibleValues.containsKey(value)) {
+
+        if (this.possibleValues.containsKey(value) && possiblePositions.containsKey(new Pair<>(x,y))) {
             return new ValidMove(id,x,y,value);
         }
 
