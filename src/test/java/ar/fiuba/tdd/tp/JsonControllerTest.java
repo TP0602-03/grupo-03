4fe6e56dbfc7a5b9060c6d32893743cc93651ed9
LucianoRM <luciano.rmna@gmail.com>
2016-10-20 14:22:32 -0300

    Added test with inshi file

--- src/test/java/ar/fiuba/tdd/tp/JsonControllerTest.java
+++ src/test/java/ar/fiuba/tdd/tp/JsonControllerTest.java
@@ -1,18 +1,18 @@
 package ar.fiuba.tdd.tp;
 
 import ar.fiuba.tdd.tp.controller.JsonFileController;
-import ar.fiuba.tdd.tp.move.InvalidMove;
-import ar.fiuba.tdd.tp.move.Move;
-import ar.fiuba.tdd.tp.move.MoveFactory;
-import ar.fiuba.tdd.tp.move.ValidMove;
+import ar.fiuba.tdd.tp.move.*;
+import ar.fiuba.tdd.tp.parser.Parser;
 import javafx.util.Pair;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
+import static junit.framework.TestCase.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -24,10 +24,11 @@ public class JsonControllerTest {
     String allValidMoves = folder + "JsonValidMoves.json";
     String allInvalidMoves = folder + "JsonInvalidMoves.json";
     String everyOtherValidMoves = folder + "JsonEveryOtherValidMove.json";
+    String inishiFile = folder + "inishiJsonControllerMovesFile.json";
 
     HashMap<String, ArrayList<String>> valuesMap = new HashMap<String, ArrayList<String>>() {
         {
-            put("num", new ArrayList<>(Arrays.asList("1","2","3")));
+            put("num", new ArrayList<>(Arrays.asList("1","2","3","4")));
         }
     };
 
@@ -37,8 +38,20 @@ public class JsonControllerTest {
 
             put(new Pair<>(0, 0), new ArrayList<>(Arrays.asList("num")));
             put(new Pair<>(0, 1), new ArrayList<>(Arrays.asList("num")));
+            put(new Pair<>(0, 2), new ArrayList<>(Arrays.asList("num")));
+            put(new Pair<>(0, 3), new ArrayList<>(Arrays.asList("num")));
             put(new Pair<>(1, 0), new ArrayList<>(Arrays.asList("num")));
             put(new Pair<>(1, 1), new ArrayList<>(Arrays.asList("num")));
+            put(new Pair<>(1, 2), new ArrayList<>(Arrays.asList("num")));
+            put(new Pair<>(1, 3), new ArrayList<>(Arrays.asList("num")));
+            put(new Pair<>(2, 0), new ArrayList<>(Arrays.asList("num")));
+            put(new Pair<>(2, 1), new ArrayList<>(Arrays.asList("num")));
+            put(new Pair<>(2, 2), new ArrayList<>(Arrays.asList("num")));
+            put(new Pair<>(2, 3), new ArrayList<>(Arrays.asList("num")));
+            put(new Pair<>(3, 0), new ArrayList<>(Arrays.asList("num")));
+            put(new Pair<>(3, 1), new ArrayList<>(Arrays.asList("num")));
+            put(new Pair<>(3, 2), new ArrayList<>(Arrays.asList("num")));
+            put(new Pair<>(3, 3), new ArrayList<>(Arrays.asList("num")));
         }
     };
 
@@ -105,6 +118,32 @@ public class JsonControllerTest {
             index++;
         }
 
+
+    }
+
+
+    @Test
+    public void testWinningInishi() throws org.json.simple.parser.ParseException, IOException,ParseException,InvalidMoveException {
+
+        JsonFileController controller = new JsonFileController(factory);
+        controller.readFile(inishiFile);
+        Parser parser = new Parser("testFiles/inshi.json");
+        Game game = parser.getGame();
+
+
+        Move move;
+
+        while (true) {
+            move = controller.getMove();
+
+            if (move == null) {
+                break;
+            }
+            assertFalse(game.validateRules());
+            game.playCell(move.getY(),move.getX(),move.getAttribute(),move.getValue());
+
+        }
+        assertTrue(game.validateRules());
 
     }
 
